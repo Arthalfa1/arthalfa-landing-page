abd4aa44b4aaf95ab97a10eebcb4fcdb44d6a11a
Arthalfa1 <jaishilhike@gmail.com>
2024-12-16 19:26:52 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect } from "react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Check, ChevronRight, BarChart2, Users, Bell, Gift, Target, LineChart, CircleDot, MessageSquare, Trophy, PieChart, Send, Heart, Share2, MessageCircle, Bookmark, Home, TrendingUp, Rss, Search, MoreHorizontal, UserPlus, ShieldCheck, Facebook, Twitter, Linkedin, Instagram } from 'lucide-react'
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Page() {
+  // Your existing LandingPage component code here
+  // Make sure to replace all instances of "FinanceGrow" with "Arthalfa"
+}
+
